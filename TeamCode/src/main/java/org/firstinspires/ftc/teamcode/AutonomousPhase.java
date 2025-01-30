766174ae407a464782435bf574224837d417f619
Dranoel <dranoel512@proton.me>
2025-01-30 08:55:57 +0000

    Add empty autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousPhase.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Config
+@Autonomous(name = "Autonomous Phase", group = "Into the Deep")
+public class AutonomousPhase extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+    }
+}
